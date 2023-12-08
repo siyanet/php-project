0c88ac47e68516ffb8b0fd28f843f7f74dea970e
nebHailemariam <nebhailemariam@gmail.com>
2023-12-08 10:48:10 +0300

    validation for date is added

--- view/validate.js
+++ view/validate.js
@@ -1,10 +1,10 @@
 function validate_dob(){
     let dob = new Date(document.getElementById("date_birth").value);
-    let now = new Date();
-    if (dob >= now){
-        alert("please select a date before the current date");
+    let fiveyears = new Date();
+     fiveyears.setFullYear(fiveyears.getFullYear()-5);
+    if (dob >= fiveyears){
+        alert("you are not young enough");
         return false;
     }
-    else{
-    return true;}
+   return true;
 }
